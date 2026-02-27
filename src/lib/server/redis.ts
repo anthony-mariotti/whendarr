0e6cee69c56ecd26c352e899916c5b17d9407dff
Anthony Mariotti <git@anthonymariotti.com>
2026-02-27 15:02:02 -0500

    prevent double initialization

--- src/lib/server/redis.ts
+++ src/lib/server/redis.ts
@@ -6,54 +6,44 @@ type RedisConnect =
     | { enabled: false; client: null };
 
 let client: Redis | null = null;
-let enabled: boolean = false;
 
 let state: RedisConnect = {
     enabled: false,
     client: null
 }
 
-if (env.REDIS_URL) {
-    try {
-        client = new redis(env.REDIS_URL);
+try {
+    client = new redis(env.REDIS_URL, {
+        enableOfflineQueue: false,
+    });
 
-        // client = createClient({
-        //     url: env.REDIS_URL,
-        //     socket: {
-        //         reconnectStrategy: (retries) => {
-        //             return Math.min(retries * 100, 5000);
-        //         },
-        //     }
-        // });
+    // client = createClient({
+    //     url: env.REDIS_URL,
+    //     socket: {
+    //         reconnectStrategy: (retries) => {
+    //             return Math.min(retries * 100, 5000);
+    //         },
+    //     }
+    // });
 
-        client.on('connect', () => {
-            console.log('Redis connecting...');
-            enabled = false
-        });
+    client.on('connect', () => {
+        console.log('Redis connecting...');
+    });
 
-        client.on('ready', () => {
-            console.log('Redis connected - caching enabled');
-            enabled = true
-        });
+    client.on('ready', () => {
+        console.log('Redis connected');
+    });
 
-        client.on('end', () => {
-            console.warn('Redis disconnected - caching disabled');
-            enabled = false
-        });
+    client.on('end', () => {
+        console.warn('Redis disconnected...');
+    });
 
-        client.on('error', () => {
+    client.on('error', () => {
 
-        });
+    });
 
-        await client.connect();
-        console.log('Redis Caching Enabled');
-    } catch (err) {
-        console.error('Redis failed to initialize, continuing eithout cache')
-        console.error('Redis', err);
-        enabled = false;
-    }
-} else {
-    console.log('Redis disabled (no REDIS_URL)');
+} catch (err) {
+    console.error('Redis', err);
 }
 
 export {
