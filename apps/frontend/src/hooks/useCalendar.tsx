cdfeab45b5abee2b001025e4b8ba93b7e1b269a0
Anthony Mariotti <git@anthonymariotti.com>
2026-05-09 16:08:38 -0400

    use callback for specific re-rendering when changed views

--- apps/frontend/src/hooks/useCalendar.tsx
+++ apps/frontend/src/hooks/useCalendar.tsx
@@ -1,5 +1,5 @@
 import dayjs from 'dayjs';
-import { createContext, useContext, useState } from 'react';
+import { createContext, useCallback, useContext, useState } from 'react';
 
 export const CALENDAR_VIEWS = ['month', 'week', 'day'] as const;
 export type CalendarViewMode = (typeof CALENDAR_VIEWS)[number];
@@ -48,80 +48,89 @@ export function CalendarProvider({ children, ...props }: CalendarProviderProps) 
   const [view, setView] = useState<CalendarViewMode>(initialState.view);
   const [filters, setFilters] = useState<CalendarState['filter']>(initialState.filter);
 
+  const prevPeriod = useCallback(() => {
+    switch (view) {
+      case 'month':
+        setMonth((m) => {
+          const next = m.subtract(1, 'month');
+          setSelectedDay(next.endOf('month'));
+          return next;
+        });
+        break;
+      case 'week':
+        setSelectedDay((d) => {
+          const next = d.subtract(1, 'week').startOf('week');
+          setMonth(next.startOf('month'));
+          return next;
+        });
+        break;
+      case 'day':
+        setSelectedDay((d) => {
+          const next = d.subtract(1, 'day');
+          setMonth(next.startOf('month'));
+          return next;
+        });
+        break;
+    }
+  }, [view]);
+
+  const nextPeriod = useCallback(() => {
+    switch (view) {
+      case 'month':
+        setMonth((m) => {
+          const next = m.add(1, 'month');
+          setSelectedDay(next.startOf('month'));
+          return next;
+        });
+        break;
+      case 'week':
+        setSelectedDay((d) => {
+          const next = d.add(1, 'week').startOf('week');
+          setMonth(next.startOf('month'));
+          return next;
+        });
+
+        break;
+      case 'day':
+        setSelectedDay((d) => {
+          const next = d.add(1, 'day');
+          setMonth(next.startOf('month'));
+          return next;
+        });
+
+        break;
+    }
+  }, [view]);
+
+  const today = useCallback(() => {
+    const today = dayjs();
+    setMonth(today);
+    setSelectedDay(today);
+  }, []);
+
+  const setFilter = useCallback((filter: Partial<CalendarState['filter']>) => {
+    setFilters((prev) => ({
+      ...prev,
+      ...filter
+    }));
+  }, []);
+
+  const selectDay = useCallback((day: dayjs.Dayjs) => {
+    setSelectedDay(day);
+    setMonth(day.startOf('month'));
+  }, []);
+
   const value: CalendarState = {
     month,
     selectedDay,
     view,
     filter: filters,
-    prevPeriod: () => {
-      switch (view) {
-        case 'month':
-          setMonth((m) => {
-            const next = m.subtract(1, 'month');
-            setSelectedDay(next.endOf('month'));
-            return next;
-          });
-          break;
-        case 'week':
-          setSelectedDay((d) => {
-            const next = d.subtract(1, 'week').startOf('week');
-            setMonth(next.startOf('month'));
-            return next;
-          });
-          break;
-        case 'day':
-          setSelectedDay((d) => {
-            const next = d.subtract(1, 'day');
-            setMonth(next.startOf('month'));
-            return next;
-          });
-
-          break;
-      }
-    },
-    nextPeriod: () => {
-      switch (view) {
-        case 'month':
-          setMonth((m) => {
-            const next = m.add(1, 'month');
-            setSelectedDay(next.startOf('month'));
-            return next;
-          });
-          break;
-        case 'week':
-          setSelectedDay((d) => {
-            const next = d.add(1, 'week').startOf('week');
-            setMonth(next.startOf('month'));
-            return next;
-          });
-
-          break;
-        case 'day':
-          setSelectedDay((d) => {
-            const next = d.add(1, 'day');
-            setMonth(next.startOf('month'));
-            return next;
-          });
-
-          break;
-      }
-    },
-    today: () => {
-      const today = dayjs();
-      setMonth(today);
-      setSelectedDay(today);
-    },
-    setFilter: (filter: Partial<CalendarState['filter']>) => {
-      setFilters((prev) => ({
-        ...prev,
-        ...filter
-      }));
-    },
-    setView: (view) => setView(view),
-    selectDay: (day: dayjs.Dayjs) => {
-      setSelectedDay(day);
-      setMonth(day.startOf('month'));
-    }
+    prevPeriod,
+    nextPeriod,
+    today,
+    setFilter,
+    setView,
+    selectDay
   };
 
   return (
